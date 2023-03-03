c2e6cfea4573e32178455a6550c91740c6e6633d
ut-kr <utkarsh@cyberfear.com>
2023-03-03 18:12:58 +1100

    🍩 initialize program

--- Program.cs
+++ Program.cs
@@ -7,7 +7,33 @@ namespace escapade
     {
         public static void Main()
         {
+            new Window("Camera Test", 800, 800);
+            SplashKit.HideMouse();
 
+            bool quitGame = QuitGame();
+            while (!quitGame)
+            {
+                quitGame = QuitGame();
+            }
+
+        }
+
+        // quit the game
+        private static bool QuitGame()
+        {
+            // different ways to quit the game
+        }
+
+        // Take user input and trigger specific action
+        public static void Controls()
+        {
+            
+        }
+
+        // Draw everything on the screen
+        public static void Draw()
+        {
+            
         }
     }
 }
