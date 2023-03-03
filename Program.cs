096eb0e83c4a9dbfbe49ddd479d2a42a6180b0e3
ut-kr <utkarsh@cyberfear.com>
2023-03-03 18:24:36 +1100

    ✨ Added quit capabilities

--- Program.cs
+++ Program.cs
@@ -7,13 +7,19 @@ namespace escapade
     {
         public static void Main()
         {
-            new Window("Camera Test", 800, 800);
+            new Window("escapade", 1500, 900);
             SplashKit.HideMouse();
 
             bool quitGame = QuitGame();
             while (!quitGame)
             {
+                SplashKit.ProcessEvents(); // Handle input to adjust player movement
                 quitGame = QuitGame();
+                SplashKit.ClearScreen();   // clears the screen
+                
+                // code
+                
+                SplashKit.RefreshScreen(60);
             }
 
         }
@@ -21,7 +27,7 @@ namespace escapade
         // quit the game
         private static bool QuitGame()
         {
-            // different ways to quit the game
+            return (SplashKit.KeyDown(KeyCode.QKey) || SplashKit.QuitRequested());
         }
 
         // Take user input and trigger specific action
