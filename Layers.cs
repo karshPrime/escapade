ecf00e6d057ffdae435c38dcc2aad377e26f5c34
ut-kr <utkarsh@cyberfear.com>
2023-03-03 18:13:15 +1100

    🍩 initialize layers

---
+++ Layers.cs
@@ -0,0 +1,21 @@
+using SplashKitSDK;
+
+namespace escapade;
+
+public class Layers
+{
+    public Layers()
+    {   // initialize all different layers
+        //
+    }
+
+    public void Move(bool direction)
+    {   // direction 1 is forward and 0 is backward
+        //
+    }
+
+    public void Draw()
+    {   // draw all layers on the screen
+        //
+    }
+}
