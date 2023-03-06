c8445b39effadfe285b050c797a7983d27708959
ut-kr <utkarsh@cyberfear.com>
2023-03-06 20:07:45 +1100

    ✨ initial config
    added basic staff methods.

--- SrcEntities/Staff.cs
+++ SrcEntities/Staff.cs
@@ -4,4 +4,31 @@ namespace Escapade.SrcEntities;
 
 public class Staff : Layer
 {
+    private float _ground;
+    private bool _isShooting;
+    private float[] _target = new float[2];
+
+    public Staff() : base("staff", 2)
+    {
+        _isShooting = false;
+    }
+
+    public void Hover()
+    {
+        // update staff's _cordsY for bounce on _ground
+    }
+
+    public void UpdateGround(float playerY)
+    {
+        _ground = playerY;
+    }
+
+    public void Shooting()
+    {
+        if (_isShooting)
+        {
+            // perform shooting animation and attack action
+            // end with making _isShooting = false;
+        }
+    }
 }
