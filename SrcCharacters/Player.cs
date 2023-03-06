14312022de4002fee2d0d4f85412448af005208c
ut-kr <utkarsh@cyberfear.com>
2023-03-07 09:16:48 +1100

    ✨ player update

--- SrcCharacters/Player.cs
+++ SrcCharacters/Player.cs
@@ -4,22 +4,35 @@ public class Player : Characters
 {
     private byte _kills;
     private bool _isJumping;
-
+    private float _fallSpeed;
+    
     public Player()
         : base(200, 0, 0, "zul")
     {
         _kills = 0;
         _isJumping = false;
+        _fallSpeed = 1;
     }
 
     public void Jump()
     {
-        // initiate jump if _isJumping = false
+        // initiate jump if not already jumping
+        if (!_isJumping) { _isJumping = true; }
     }
 
     public void Jumping()
     {
-        // update character's movement is _isJumping = true
+        if (_isJumping)
+        {
+            cordsY -= _fallSpeed;
+            // update character's movement is _isJumping = true
+            // end with changing _isJumping back to false
+        }
+    }
+
+    private float CurrentGround()
+    {
+        return 1.3f; // change this value. is random for now.
     }
 
     public override void Death()
